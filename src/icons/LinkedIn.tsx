2218547d824dcbde3e6dffa553b9bf3391a4a8f3
tigerabrodi <tigerabrodi@gmail.com>
2022-09-07 09:32:11 +0200

    feat: add site for mobile

---
+++ src/icons/LinkedIn.tsx
@@ -0,0 +1,17 @@
+export function LinkedIn() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 29 29">
+      <g clip-path="url(#linkedin)">
+        <path
+          fill="#080A0A"
+          d="M28.993 29H29V18.36c0-5.201-1.12-9.21-7.203-9.21-2.925 0-4.886 1.605-5.688 3.126h-.087V9.637H10.26V29h6.006v-9.588c0-2.525.479-4.966 3.604-4.966 3.08 0 3.126 2.882 3.126 5.129V29h6-.002ZM.479 9.639H6.49V29H.479V9.639ZM3.482 0A3.483 3.483 0 0 0 0 3.482c0 1.923 1.559 3.516 3.482 3.516 1.923 0 3.483-1.593 3.483-3.516A3.488 3.488 0 0 0 3.48 0h.002Z"
+        />
+      </g>
+      <defs>
+        <clipPath id="linkedin">
+          <path fill="#fff" d="M0 0h29v29H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
--- src/sections/home/Home.tsx
+++ src/sections/home/Home.tsx
@@ -1,7 +1,80 @@
+import { NarutoFist } from '../../arts/NarutoFist'
+import { Github } from '../../icons/Github'
+import { LinkedIn } from '../../icons/LinkedIn'
+import { Pen } from '../../icons/Pen'
+import { Twitter } from '../../icons/Twitter'
 import './Home.css'
 
 export function Home() {
   return (
-      <div>hello world</div>
+    <main class="main">
+      <NarutoFist class="art" />
+      <h1 class="title">Tiger Abrodi</h1>
+      <h2 class="subtitle">
+        Senior Software Engineer at{' '}
+        <a
+          href="https://bobsled.co"
+          class="subtitle-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bobsled
+        </a>
+      </h2>
+      <div class="texts">
+        <p class="text">
+          I love programming. It is truly a craft to me, one which combines
+          structure and art. I love building things for the web, taking ideas to
+          things that people can use.
+        </p>
+        <p class="text">
+          In my spare time, I build side projects, write blog posts and read
+          books. I also design using Figma, and have a passion for both
+          designing and building accessible experiences.
+        </p>
+        <p class="text">
+          I work across the full stack with an emphasis on the frontend.
+        </p>
+        <p class="text">
+          At the moment, my learning evolve around AWS, Solid.js, Distributed
+          Systems and Terraform.
+        </p>
+      </div>
+
+      <div class="social-links">
+        <a
+          href="https://www.linkedin.com/in/tiger-abrodi-467695195/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="social-link"
+        >
+          <LinkedIn />
+        </a>
+        <a
+          href="https://twitter.com/TAbrodi"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="social-link"
+        >
+          <Twitter />
+        </a>
+        <a
+          href="https://github.com/narutosstudent"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="social-link"
+        >
+          <Github />
+        </a>
+        <a
+          href="https://tigerabrodi.hashnode.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="social-link"
+        >
+          <Pen />
+        </a>
+      </div>
+    </main>
   )
 }
